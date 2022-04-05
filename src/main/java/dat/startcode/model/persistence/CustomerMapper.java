978839c68e145c6f5a05146a5ba561370304f856
OwaisAD <owaisdashti@outlook.com>
2022-04-05 12:33:21 +0200

    tilføjet servlet klasser og jsp sider

--- src/main/java/dat/startcode/model/persistence/CustomerMapper.java
+++ src/main/java/dat/startcode/model/persistence/CustomerMapper.java
@@ -1,5 +1,8 @@
 package dat.startcode.model.persistence;
 
+import dat.startcode.model.entities.Cupcake;
+import dat.startcode.model.entities.Customer;
+import dat.startcode.model.entities.Order;
 import dat.startcode.model.entities.User;
 import dat.startcode.model.exceptions.DatabaseException;
 
@@ -19,67 +22,29 @@ public class CustomerMapper implements ICustomerMapper
         this.connectionPool = connectionPool;
     }
 
+
     @Override
-    public User login(String username, String password) throws DatabaseException
-    {
-        Logger.getLogger("web").log(Level.INFO, "");
-
-        User user = null;
-
-        String sql = "SELECT * FROM user WHERE username = ? AND password = ?";
-
-        try (Connection connection = connectionPool.getConnection())
-        {
-            try (PreparedStatement ps = connection.prepareStatement(sql))
-            {
-                ps.setString(1, username);
-                ps.setString(2, password);
-                ResultSet rs = ps.executeQuery();
-                if (rs.next())
-                {
-                    String role = rs.getString("role");
-                    user = new User(username, password, role);
-                } else
-                {
-                    throw new DatabaseException("Wrong username or password");
-                }
-            }
-        } catch (SQLException ex)
-        {
-            throw new DatabaseException(ex, "Error logging in. Something went wrong with the database");
-        }
-        return user;
+    public Customer createProfile(String email, String password) throws DatabaseException {
+        return null;
     }
 
     @Override
-    public User createUser(String username, String password, String role) throws DatabaseException
-    {
-        Logger.getLogger("web").log(Level.INFO, "");
-        User user;
-        String sql = "insert into user (username, password, role) values (?,?,?)";
-        try (Connection connection = connectionPool.getConnection())
-        {
-            try (PreparedStatement ps = connection.prepareStatement(sql))
-            {
-                ps.setString(1, username);
-                ps.setString(2, password);
-                ps.setString(3, role);
-                int rowsAffected = ps.executeUpdate();
-                if (rowsAffected == 1)
-                {
-                    user = new User(username, password, role);
-                } else
-                {
-                    throw new DatabaseException("The user with username = " + username + " could not be inserted into the database");
-                }
-            }
-        }
-        catch (SQLException ex)
-        {
-            throw new DatabaseException(ex, "Could not insert username into database");
-        }
-        return user;
+    public Customer login(String email, String password) throws DatabaseException {
+        return null;
     }
 
+    @Override
+    public Order placeOrder(Cupcake cupcake, int amount) throws DatabaseException {
+        return null;
+    }
 
+    @Override
+    public boolean payForOrder(int totalPriceForCupcakes, int customerCredit) throws DatabaseException {
+        return false;
+    }
+
+    @Override
+    public Order checkOrder() throws DatabaseException {
+        return null;
+    }
 }
